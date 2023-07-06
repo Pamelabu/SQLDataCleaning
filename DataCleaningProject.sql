749bb57e236769432af97b35a53cfccf6cc07dc9
Pamelabu <117838845+Pamelabu@users.noreply.github.com>
2023-07-06 23:13:56 +0200

    Add files via upload

---
+++ DataCleaningProject.sql
@@ -0,0 +1,153 @@
+
+SELECT * 
+FROM PortfolioProject.dbo.NashvilleHousing
+
+------------------------------------------------
+
+--Standarize Date Format
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+ALTER COLUMN SaleDate Date
+
+------------------------------------------------
+
+--Populate Property Address data
+
+SELECT * 
+FROM PortfolioProject.dbo.NashvilleHousing
+--WHERE PropertyAddress is null
+ORDER BY ParcelID
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+
+UPDATE a
+SET PropertyAddress = ISNULL(a.PropertyAddress, b.PropertyAddress)
+FROM PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+------------------------------------------------
+
+--Breaking out Address Into Individual Columns (Address, City, State)
+
+SELECT PropertyAddress
+FROM PortfolioProject.dbo.NashvilleHousing
+--WHERE PropertyAddress is null
+--ORDER BY ParcelID
+
+SELECT SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1) as Address,
+SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress)) as City
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress)-1)
+
+
+ALTER TABLE NashvilleHousing
+ADD PropertySplitCity NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress)+1, LEN(PropertyAddress))
+
+---------------------------------------------
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+
+SELECT 
+PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 3),
+PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 2),
+PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 1)
+FROM PortfolioProject.dbo.NashvilleHousing
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 3)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 2)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState NVARCHAR(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',' ,'.'), 1)
+
+------------------------------------------------
+
+--Change Y and N to YES and no in "Sold as Vacant" field
+
+SELECT DISTINCT(SoldAsVacant), Count(SoldAsVacant) AS SoldAsVacantCount
+FROM PortfolioProject.dbo.NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+WHEN SoldAsVacant = 'N' THEN 'No'
+ELSE SoldAsVacant
+END
+FROM PortfolioProject.dbo.NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+WHEN SoldAsVacant = 'N' THEN 'No'
+ELSE SoldAsVacant
+END
+
+
+------------------------------------------------
+
+--Remove Duplicates
+
+WITH RowNumCTE AS
+(
+	SELECT*,
+	ROW_NUMBER() OVER(
+	PARTITION BY ParcelID,
+	PropertyAddress,
+	SalePrice,
+	SaleDate,
+	LegalReference
+	ORDER BY UniqueID
+	) AS row_num
+FROM PortfolioProject.dbo.NashvilleHousing
+)
+
+--DELETE FROM RowNumCTE WHERE row_num>1
+
+SELECT*
+FROM  RowNumCTE
+WHERE row_num>1
+
+
+
+------------------------------------------------
+
+--Delete Unused Columns
+
+SELECT *
+FROM PortfolioProject.dbo.NashvilleHousing
+			
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress
